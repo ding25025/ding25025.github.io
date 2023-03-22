bdc093e44c3639bc0c886c645be9adfadef9e186
yutingwu <ding25025@gmail.com>
2023-03-22 22:05:05 +0800

    rebuild  website

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220717.230910';
+const cacheName = 'chirpy-20230322.220311';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
