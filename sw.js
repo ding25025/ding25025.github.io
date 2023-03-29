e9b940badaeb81657049d855888264b04f534fc4
yutingwu <ding25025@gmail.com>
2023-03-29 21:23:54 +0800

    fix about content

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230329.204336';
+const cacheName = 'chirpy-20230329.211504';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
