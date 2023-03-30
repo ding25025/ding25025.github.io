79fe090e2592521cc069241dc28e9f7b89a215d9
yutingwu <ding25025@gmail.com>
2023-03-30 23:04:17 +0800

    2FA add sequence diagram

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230329.222912';
+const cacheName = 'chirpy-20230330.205522';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
