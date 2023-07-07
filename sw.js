5f24d0af4ce5cc8033182bda60369fc8a1691eab
yutingwu <ding25025@gmail.com>
2023-07-07 13:20:31 +0800

    add upgrade Apache2 version post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230330.205522';
+const cacheName = 'chirpy-20230707.131704';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
