89b16c7baad81c0e9f9678e01f2bb7f175da22a8
yutingwu <ding25025@gmail.com>
2023-07-15 16:28:42 +0800

    add azure certified post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230707.131704';
+const cacheName = 'chirpy-20230715.162722';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
