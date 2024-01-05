2ad8e598c85b0c95e8602853a6d3f3de24195f10
yutingwu <ding25025@gmail.com>
2024-01-05 18:12:33 +0800

    feat(share-google-interview): create google interview article

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230715.164448';
+const cacheName = 'chirpy-20240105.181121';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
