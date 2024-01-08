1903030dffac2f85d3fae9b88d53d6597b9e89cc
yutingwu <ding25025@gmail.com>
2024-01-08 12:03:41 +0800

    fix(share-google-interview): fix content

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240105.181121';
+const cacheName = 'chirpy-20240108.120151';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
